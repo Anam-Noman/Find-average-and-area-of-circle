413b62f0ac30bb9ad0d300ed66e153fed9f097d7
Anam-Noman <156789900+Anam-Noman@users.noreply.github.com>
2024-01-31 21:31:35 +0500

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,3 @@
+r=float(input("Enter the radius of circle"))
+area=3.14156*r*r
+print("area of circle is",area)
---
+++ Average.py
@@ -0,0 +1,9 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1=int(input("enter a number"))
+
+=================================================== RESTART: C:/Users/Hasham Sheikh/Desktop/Average.py ==================================================
+Enter first number40
+Enter second number20
+Enter third number20
+26.666666666666668
